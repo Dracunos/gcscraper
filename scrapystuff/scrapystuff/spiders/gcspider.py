54ca6e4a48d51f70761f57225bd1482947f44a77
Dracunos <markalexander72@gmail.com>
2017-03-01 15:49:53 -0500

    Added crawling functionality to spider

--- scrapystuff/scrapystuff/spiders/gcspider.py
+++ scrapystuff/scrapystuff/spiders/gcspider.py
@@ -1,16 +1,30 @@
 import scrapy
 
 
+def checklink(link):
+    if link == "../":
+        return False
+    if link[-1] == '/' or (link[-4:] == ".txt" and link[:6] == 'morgue'):
+        return True
+    return False
+  
+def checkmorgue(morgue):
+    return
+
 class GCSpider(scrapy.Spider):
     name = "gcspider"
 
-    start_urls = [#'https://crawl.project357.org/morgue/',
-                  'https://crawl.project357.org/morgue/Dracunos/morgue-Dracunos-20170301-043110.txt']
-
+    start_urls = ['https://crawl.project357.org/morgue/']
+    
     def parse(self, response):
+    
+        if response.url[-4:] == '.txt':
+            print response.body
+            return
 
-        print response.body
-
-        #next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        #if next_page_url is not None:
-        #    yield scrapy.Request(response.urljoin(next_page_url))
+        hrefs = response.css("a::attr(href)").extract()
+        
+        links = [response.urljoin(x) for x in hrefs if checklink(x)]
+        
+        for link in links:
+            yield scrapy.Request(link)
